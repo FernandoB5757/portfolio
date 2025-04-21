4109d5d665d9233e795fe2e5c22fd3a5f504076d
Jesus Fernando Barajas Galindo <jesusfernando@netcommerce.mx>
2025-04-21 15:39:24 -0600

    docs: types orbit

---
+++ types/orbit.ts
@@ -0,0 +1,24 @@
+type ObjectValues<T> = T[keyof T];
+
+export const ORBIT_DIRECTION = {
+  Clockwise: "normal",
+  CounterClockwise: "reverse",
+} as const;
+
+export type OrbitDirection = ObjectValues<typeof ORBIT_DIRECTION>;
+
+export interface Props {
+  class?: string;
+  direction?: OrbitDirection;
+  duration?: number;
+  delay?: number;
+  radius?: number;
+  path?: boolean;
+}
+
+export interface OrbitTechnology extends Props {
+  slug: string,
+  icon?: string
+}
+
+export { default as Orbit } from "@/components/animations/Orbit.vue";
