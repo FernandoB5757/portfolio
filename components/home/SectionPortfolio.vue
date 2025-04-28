c0001cf1ee4093f8982ba9843fd2f10f473249b1
Jesus Fernando Barajas Galindo <jesusfernando@netcommerce.mx>
2025-04-28 15:56:02 -0600

    feature: new component SectionPortfolio

---
+++ components/home/SectionPortfolio.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="py-16 relative ml-auto mr-auto flex">
+        <div class="flex relative w-full p-3 content-center flex-wrap ">
+            <div class="relative w-full">
+                <div class="text-center" id="my-portfolio-header">
+                    <BlurReveal
+                        :delay="0.2"
+                        :duration="0.75"
+                        class="p-8"
+                    >
+                        <span class="text-primary block uppercase text-sm font-medium tracking-widest leading-4">
+                            Visit my portfolio and keep your feedback
+                        </span>
+                        <h2 class="text-6xl font-bold leading-tight  mt-4 mb-0">
+                            My Portfolio
+                        </h2>
+                    </BlurReveal>
+                </div>
+
+                <div id="my-portfolio-projects" class="w-full relative">
+                    <div class="flex">
+                        <div>wewqewq</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import BlurReveal from '../animations/BlurReveal.vue';
+import { useTheme } from '~/composables/useTheme'
+
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
+    <div class="bg-surface-100 dark:bg-surface-900">
         <section id="welcome-home">
             <SectionHome />
         </section>
         
-        <section id="about-me" class="bg-surface-100 dark:bg-surface-900">
+        <section id="about-me">
             <SectionAboutMe />
+        </section>
+
+        <section id="my-portfolio">
+            <SectionPortfolio />
         </section>
     </div>
 </template>
 <script setup lang="ts">
 import SectionHome from '@/components/home/SectionHome.vue'
 import SectionAboutMe from '@/components/home/SectionAboutMe.vue'
+import SectionPortfolio from '@/components/home/SectionPortfolio.vue'
 
 </script>
