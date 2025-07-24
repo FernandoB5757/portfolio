af51ecff387f1242ad773161b2abe067917a0ffa
Jesus Fernando Barajas Galindo <jesusfernando@netcommerce.mx>
2025-07-24 17:06:02 -0600

    restore default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,21 @@
 <template>
-    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
-        <main>
-            <AppHeader />
-            <slot />
-            <ScrollTop />
-            <footer>
-                <AppFooter />
-            </footer>
-             <Icon name="heroicons:computer-desktop" class="!hidden" />
-            <Icon name="heroicons:sun" class="!hidden" />
-            <Icon name="heroicons:moon" class="!hidden" />
-            <Icon name="heroicons:x-mark" class="!hidden" />
-            <Icon name="heroicons:arrow-right-circle" class="!hidden" />
+    <main>
+        <AppHeader />
+        <slot />
+        <ScrollTop />
+        <footer>
+            <AppFooter />
+        </footer>
+            <Icon name="heroicons:computer-desktop" class="!hidden" />
+        <Icon name="heroicons:sun" class="!hidden" />
+        <Icon name="heroicons:moon" class="!hidden" />
+        <Icon name="heroicons:x-mark" class="!hidden" />
+        <Icon name="heroicons:arrow-right-circle" class="!hidden" />
 
-            <Icon name="circle-flags:us-um" class="!hidden" />
-            <Icon name="circle-flags:mx" class="!hidden"  />
-        </main>
-    </Html>
+        <Icon name="circle-flags:us-um" class="!hidden" />
+        <Icon name="circle-flags:mx" class="!hidden"  />
+    </main>
 </template>
 
 <script setup>
-const head = useLocaleHead()
 </script>
